d32e8a7bec46a16b2be4d5b9e3d397af1d665c98
starlightlacuna <169253492+starlightlacuna@users.noreply.github.com>
2024-07-21 01:25:34 -0400

    Add ending dialogue

--- scripts/level_select.gd
+++ scripts/level_select.gd
@@ -2,18 +2,25 @@ extends Control
 
 func _ready():
 	var parts = PlayerInventory.get_parts()
+	var complete = true
+	for key in parts:
+		if parts[key] == false:
+			complete = false
+	if complete:
+		Dialogic.start("All Parts Collected")
+		$MarginContainer.set_visible(false)
+	
 	_get_button("SpringButton").set_disabled(parts["Spring"])
 	_get_button("SummerButton").set_disabled(parts["Summer"])
 	_get_button("AutumnButton").set_disabled(parts["Autumn"])
 	_get_button("WinterButton").set_disabled(parts["Winter"])
-	
-	# TODO: If all parts have been collected, play the ending dialogue!
 
 func _get_button(button_name: String) -> TextureButton:
 	return $MarginContainer/GridContainer.get_node(button_name)
 
 func _on_spring_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/spring/spring_level.tscn")
+
 func _on_summer_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/summer/summer_level.tscn")
 
